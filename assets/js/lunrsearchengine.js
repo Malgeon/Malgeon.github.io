f6a9f110ec0295d8e82b7bbe940de97df42ec39b
Malgeon <nyangsun@gmail.com>
2020-12-08 07:42:54 +0000

    Updating to Malgeon/makeBlog@3aa79784b486088a76574e1ab5700ce32fa1ea58.

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -488,7 +488,7 @@ var documents = [{
     "id": 97,
     "url": "/android_app_service_clone/",
     "title": "Movie Clone Application",
-    "body": "2020/11/23 - API: 주어진 API 상세 : 앱 아키텍처: "
+    "body": "2020/11/23 - API: 주어진 API 상세 : 앱 아키텍처: 아래와 같이 안드로이드 개발자 페이지에서 추천하는 아키텍쳐로 구성하였다.  MVVM: 디자인 아키텍쳐는 MVVM로 만들고자 했다. DI - Koin: 다음 프로젝트에서는 Dagger-Hilt를 사용할 예정이다. NetWork - rxJava, retrofit: 현재까지 이해하기로, API를 통해 Data를 받고자 할 때 비동기로 처리하기 위한 방법으로 rxJava를 사용하고 있으며 위 프로젝트에서는 API 데이터가 많지 않아 단순히 Single로 발행하고 있다. (Observable로 구성하여 rxBus를 만들수 있어서 확장성 측면에서 Observable로 구성하는 것이 연습하는 목적에선 더 좋을것 같긴한데, 우선 하나 프로젝트를 끝내고 싶은 마음에 시간 관계상 다음 프로젝트에서 할 예정이다. ) API통신 방법은 volley보단 retrofit이 표준인 것 같다. "
     }, {
     "id": 98,
     "url": "/study_Kotlin_practice_10/",
